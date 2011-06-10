954057064f8f5401e081d1ae0d894424c5f4a087
Conroy Whitney <conroy.whitney@gmail.com>
2011-06-10 12:09:53 -0400

    removing extraneous brackets from season list order by

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -2,7 +2,7 @@ class SeasonController < ApplicationController
 
   def show
     @season = params[:season]
-    @episodes = Episode.find_all_by_season(@season, :order => ["episode ASC"])
+    @episodes = Episode.find_all_by_season(@season, :order => "episode ASC")
   end
 
 end
