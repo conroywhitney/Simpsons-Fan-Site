93594e0e8126f03cc5bc36ac8ab92a0e07a307c8
Conroy Whitney <conroy.whitney@gmail.com>
2011-06-11 18:51:05 -0400

    adding current_url helper for fb:like

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+    def current_url(overwrite={})
+        url_for :only_path => false, :params => params.merge(overwrite)
+    end
+
 end
